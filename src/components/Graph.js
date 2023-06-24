3a3aed99b818a26194ea10dbf56535bd3af3ab11
fanel_byte <jf_bentaleb@esi.dz>
2023-06-24 18:53:04 +0100

    version with graph working

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,41 +1,89 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { format } from "d3-format";
 import {
-  LineChart,
-  Line,
+  XYPlot,
   XAxis,
   YAxis,
-  CartesianGrid,
-  Tooltip,
-  ResponsiveContainer,
-} from "recharts";
+  HorizontalGridLines,
+  VerticalGridLines,
+  LineMarkSeries
+} from "react-vis";
 
 const Graph = () => {
-  const [signalements, setSignalements] = useState([
-    { hour: "08h", count: 12 },
-    { hour: "010h", count: 15 },
-    { hour: "12h", count: 3 },
-    { hour: "14h", count: 1 },
-    { hour: "16h", count: 15 },
-    { hour: "18h", count: 1 },
-    { hour: "20h", count: 10 },
-    // ... (remaining data entries)
-  ]);
+  const [signalements, setSignalements] = useState([]);
+  const [maxSignalements, setMaxSignalements] = useState(1);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:4000/signalement/getNumberSignalementsByHour');
+        console.log("res", response);
+        const data = response.data.map((item) => ({
+          x: item.hour,
+          y: item.signalement
+        }));
+        setSignalements(data);
+        const max = Math.max(...response.data.map(item => item.signalement));
+        setMaxSignalements(max);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
-    <div className="shadow border-gray-500 mr-5 ml-auto mt-2 pt-5 pb-4 pr-5 rounded-md  w-1/2">
+    <div className="shadow border-gray-500 mr-5 ml-auto pt-5 pb-4 pr-5 rounded-md w-1/2">
       <div className="flex flex-col justify-between">
-        <h2 className="text-xl text-right font-semibold  mb-2">معدل الاخطارات اليومي</h2>
+        <h2 className="text-xl text-right font-semibold mt-1 mb-1">معدل الاخطارات اليومي</h2>
 
-        <div className="flex flex-grow h-40">
-          <ResponsiveContainer >
-            <LineChart data={signalements}>
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="hour" />
-              <YAxis />
-              <Tooltip />
-              <Line type="monotone" dataKey="count" stroke="#FF2F2F" />
-            </LineChart>
-          </ResponsiveContainer>
+        <div className="flex flex-grow h-40 w-full">
+          <XYPlot
+            xDomain={[8, 24]}
+            yDomain={[0, maxSignalements]}
+            width={570}
+            height={170}
+          >
+            <HorizontalGridLines
+              style={{
+                stroke: 'gray',
+                strokeWidth: 0.5,
+              }}
+            />
+            <VerticalGridLines  className="axis-x border-gray-200"
+              style={{
+                stroke: 'gray',
+                strokeWidth: 0.5,
+              }}
+            />
+            <XAxis
+          
+              className="axis-x border-gray-200"
+              tickValues={[...Array(17).keys()].map(i => i + 8)}
+              tickFormat={(v) => (v === 24 ? "24h " : `${v}h`)}
+            />
+            <YAxis
+              className="axis-y  border-gray-200"
+              tickTotal={Math.min(5, maxSignalements)}
+              tickFormat={(v) => Math.round(v)}
+            />
+           <LineMarkSeries
+  className="linemark-series-example"
+  style={{
+    strokeWidth: '2px',
+    fill: 'transparent',
+  }}
+  lineStyle={{ stroke: '#FF2F2F' }}
+  markStyle={{
+    stroke: '#FF2F2F',
+    size: 2, // Adjust the size of the circles
+  }}
+  curve="curveMonotoneX"
+  data={signalements}
+/>
+
+          </XYPlot>
         </div>
       </div>
     </div>
